bbbbf2fd0fcbe5652a27a44803bb75d9837b5ef9
tjebo <tjebn@web.de>
2020-01-19 16:09:06 +0000

    update documentation and namespace

--- R/tjebutils.R
+++ R/tjebutils.R
@@ -40,27 +40,30 @@ count_pateyes <- function(data, id = NULL, eye = NULL, text = FALSE){
 
 
 #' get_age
-#' @author Moody_Mudskipper, modified from dyears() to years()
+#' @author SO::Moody_Mudskipper, mildly modified
+#' @description calculates age in years, either as duration or as period
+#' @param from_date start date
+#' @param to_date end date
+#' @param period default FALSE: output as a duration. If TRUE, output as a period
 #' @export
 
-get_age <- function(from_date,to_date = lubridate::now(),dec = FALSE){
+get_age <- function(from_date, to_date = lubridate::now(), period = FALSE){
   if(is.character(from_date)) from_date <- lubridate::as_date(from_date)
   if(is.character(to_date))   to_date   <- lubridate::as_date(to_date)
-  if (dec) { age <- lubridate::interval(start = from_date, end = to_date)/lubridate::years(1)
-  } else   { age <- lubridate::year(lubridate::as.period(lubridate::interval(start = from_date, end = to_date)))}
+  if (period) { age <- lubridate::year(lubridate::as.period(lubridate::interval(start = from_date, end = to_date)))
   age
+  } else { age <- lubridate::interval(start = from_date, end = to_date)/lubridate::dyears(1)
+  age
+  }
 }
 
 
 #' show_stats
 #'
 #' @description pulls the most commonly used summary statistics
-#'
 #' @param x numeric/integer vector
-#'
 #' @return named vector
 #' @export
-
 
 show_stats <- function(x) {
   funs <- list(mean = mean, sd = sd, min = min, max = max )
@@ -81,24 +84,30 @@ show_stats <- function(x) {
 #' @author tjebo - eclectic choice of functions, mainly with the help of SO::G.Grothendieck and SO::Joshua Ulrich
 #' https://stackoverflow.com/a/4351626/7941188 and https://stackoverflow.com/a/16193667/7941188
 #'
-#' @description makes vector of dates of beginning of full (!) weeks
-#'
+#' @description Takes vector of dates and outputs floor or ceiling date of the respective week.
+#' The end of week can be defined. The output can be used e.g. for zoo aggregation
+#' @import zoo
 #' @param x Vector of dates Dataframe in which the vectors are found
-#' @param week_end desired end of week
-#'
+#' @param week_end what's the end of week? Takes english names of weekdays as quoted string, capital letters allowed
+#' @param ceiling default TRUE: last day of the week for each weekend. if FALSE, first day of week
 #' @return vector of dates. can be used in aggregations, e.g. with zoo.aggregate
 #' @export
 
-weekly <- function(x, week_end = 'sunday') {
+weekly <- function(x, week_end = 'sunday', ceiling = TRUE) {
+  week_end <- tolower(week_end)
   days.of.week <- tolower(weekdays(as.Date(3,"1970-01-01",tz="GMT") + 0:6))
-  index = which(days.of.week == week_end)-1
-  7 * ceiling(as.numeric(x - index + 4)/7) + zoo::as.Date(index - 4)
+  if(ceiling) {
+    index = which(days.of.week == week_end)-1
+    vec <- 7 * ceiling(as.numeric(x - index + 4)/7) + zoo::as.Date(index - 4)
+  vec
+  } else {index = which(days.of.week == week_end)
+  vec <- 7 * floor(as.numeric(x - index + 4)/7) + zoo::as.Date(index - 4)}
+  vec
 }
 
 #' UK_bankholidays
 #'
 #' @author  modified function timeDate::holidayLONDON - for description see ?timeDate::holidayLONDON
-#'
 #' @description includes NAMES of bank holidays in output vector
 #' @import timeDate
 #' @return vector of POSIX.
@@ -258,9 +267,11 @@ anonymize <- function(df) {
   return(df)
 }
 
-
 #' csv
+#' @description wrapper around write.csv with default 'row.names = FALSE'
 #' @name csv
+#' @param x data frame
+#' @param name name of csv file (does not need .csv extension), (quoted) string. If not specified, the name of the data frame will be used
 #' @export
 
 csv <- function(x, name = deparse(substitute(x))) {
